ea59f44ec85a847cfa1b12b6c804e5d2b0823b0d
Hotbasin <registrator@hotbasin.ru>
2022-02-16 15:43:16 +0800

    Workday fix2 16-02-2022

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-from bottle import route, run, HTTPError
+from bottle import route, run, request, redirect, HTTPError
+
+import FigClasses as fc_
 
 #####=====----- Variables -----=====#####
 
@@ -14,10 +16,19 @@ def server_root():
         output_ = f_.read()
     return output_
 
+@route('/figure', method='POST')
+def figure():
+    input_encoding = 'utf-8'
+    suburl_ = '/' + str(request.forms.get('figtype'))
+    redirect('/figure' + suburl_)
+
+@route('/figure/<class_>')
+def figure_class(class_):
+    return 'It works'
+
 #####=====----- MAIN -----=====#####
 
 if __name__ == '__main__':
     run(host='127.0.0.1', port=8080, debug=True)
-    pass
 
 #####=====----- THE END -----=====########################################
